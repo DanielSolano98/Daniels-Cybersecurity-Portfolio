3fbb94750d95f5543f9c20e2961dc3812bce7826
DanielSolano98 <139937504+DanielSolano98@users.noreply.github.com>
2024-12-16 20:22:52 -0500

    Add files via upload

---
+++ username generator.py
@@ -0,0 +1,11 @@
+print("Welcome to the username generator!")
+#ask the user what their first name is
+firstname = input("What is your first name?")
+#ask the user what their last name is
+lastname = input("What is your last name??")
+#ask the user what year they were born
+year = input("What year were you born?")
+#combine the country and pet name
+print("Your username is : " + firstname +" "+ lastname + " " + year)
+#display the band name
+
